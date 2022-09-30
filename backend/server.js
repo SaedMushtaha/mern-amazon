a60c10a9b45a7723563228ae5ccf2161c7130ab5
S.A.E.D <S.A.E.D@DESKTOP-7H26SAC>
2022-09-30 13:34:21 +0300

    add frontend and backend and fetch the data

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import data from './data.js';
+
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+const app = express();
+
+
+
+// test
+app.get('/api/products', (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
+  res.send(data.products);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
+
+var cors = require('cors');
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
+   
+//app.use(cors()); // Use this after the variable declaration
