294c293bfe131ecd3a6b0476bacb7607bcd6af5e
S.A.E.D <S.A.E.D@DESKTOP-7H26SAC>
2022-10-01 16:18:58 +0300

    Connect DB, product, signin screen

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,43 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
 
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
-
-
-
-// test
-app.get('/api/products', (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*")
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
-  res.send(data.products);
-});
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
 
-var cors = require('cors');
-const corsOptions ={
-  origin:'http://localhost:3000', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
 
-   
-//app.use(cors()); // Use this after the variable declaration
+
+// var cors = require('cors');
+// const corsOptions ={
+//   origin:'http://localhost:3000', 
+//   credentials:true,            //access-control-allow-credentials:true
+//   optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
+
+
+// res.setHeader("Access-Control-Allow-Origin", "*")
+// res.setHeader("Access-Control-Allow-Credentials", "true");
+// res.setHeader("Access-Control-Max-Age", "1800");
+// res.setHeader("Access-Control-Allow-Headers", "content-type");
+// res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
